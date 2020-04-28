39dad99e6c6716c053008b25359a2062bc9117c5
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-04-28 14:05:18 +0800

    扫雷Code

---
+++ MyButton.java
@@ -0,0 +1,9 @@
+package com.games.mineSweeping;
+
+import javax.swing.JButton;
+
+public class MyButton extends JButton {
+	private static final long serialVersionUID = 1L;
+	protected int row;
+	protected int col;
+}
